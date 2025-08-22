f7ac5d80d5add3a0e4dabf0620bc9569fbbc7099
Mateusz Baginski <cziken58@gmail.com>
2025-08-22 13:20:07 +0200

    fix: code style

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -1,20 +1,19 @@
-import {
-    defineConfig
-} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
 
+// eslint-disable-next-line node/prefer-global/process
 process.env.APP_URL = 'http://localhost:8000';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        cors: true,
-    }
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.ts'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    cors: true,
+  },
 });

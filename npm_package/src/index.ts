c0ce9a885ecc4bef8f7b95c63c661e98ced6a06e
Mateusz Baginski <cziken58@gmail.com>
2026-01-11 08:37:27 +0100

    fix: oops, re-export missing public classes

--- npm_package/src/index.ts
+++ npm_package/src/index.ts
@@ -2,4 +2,12 @@ import './livewire.d';
 
 import { registerLivewireComponentHooks } from './hooks';
 
+export { ContextsRegistry } from './hooks/context/contexts-registry';
+export { EditableComponentHook } from './hooks/editable';
+export { EditorComponentHook } from './hooks/editor';
+export { CustomEditorPluginsRegistry } from './hooks/editor/custom-editor-plugins';
+export { EditorsRegistry } from './hooks/editor/editors-registry';
+export { ClassHook, registerLivewireComponentHook } from './hooks/hook';
+export { UIPartComponentHook } from './hooks/ui-part';
+
 registerLivewireComponentHooks();
